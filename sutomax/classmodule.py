2b58d5386fea068da00dd1434e8693a09c5eb48b
bictorbang <victor.wang@ensea.fr>
2025-01-20 18:15:39 +0100

    CLI version is good now

--- sutomax/classmodule.py
+++ sutomax/classmodule.py
@@ -25,6 +25,8 @@ class sutom_helper(Set):
                 self.words.add(wd)
                 self.words_len[len(word)].add(word)
 
+        self.current_word_len = 0
+        self.current_word_first = ""
         self.current_words = self.words 
         self.letters = defaultdict(int) 
     
@@ -38,17 +40,19 @@ class sutom_helper(Set):
 
     def __contains__(self, value: str):
         wd = value.strip().upper()
-        return wd in self.words
+        return wd in self.current_words
     
     def __iter__(self):
-        return iter(self.words)
+        return iter(self.current_words)
     
     def __len__(self):
-        return len(self.words)
+        return len(self.current_words)
 
     def reset(self):
         self.letters = defaultdict(int)
         self.current_words = self.words
+        self.current_word_len = 0
+        self.current_word_first = ""
 
     def new_turn(self):
         self.letters = defaultdict(int)
@@ -58,7 +62,7 @@ class sutom_helper(Set):
         print(f"Il y a {len(self.current_words)} mot(s) correspondant(s).")
         return len(self.current_words)
     
-    def guess(self):
+    def random_guess(self):
         word = random.choice(list(self.current_words))
         print(f"Essayez {word} !")
         return word
@@ -67,12 +71,14 @@ class sutom_helper(Set):
         # Première information : on restreint la recherche en
         # fonction de la longueur du mot à trouver.
         self.current_words = self.words_len[word_length] # liste
+        self.current_word_len = word_length
         
     def first_letter(self, first_letter):
         # Deuxième information : on restreint la recherche en 
         # fonction de la première lettre du mot à trouver.
         self.current_words = {word for word in self.current_words if word.startswith(first_letter)}
         self.letters[first_letter]+=1
+        self.current_word_first = first_letter
     '''
     def last_letter(self, last_letter):
         self.current_words = {word for word in self.current_words if word.endswith(last_letter)}
--- sutomax/funcmodule.py
+++ sutomax/funcmodule.py
@@ -1,13 +1,81 @@
-def guesser(helper, choice, manual_guess = ""):
+import click 
+import regex as re
+
+GUESSER_WORDS = {
+    1: ["1", "&", "GUESS", "HASARD", "RANDOM", "R"],
+    2: ["2", "é", "MANUEL", "MANUAL", "M"],
+    3: ["3", '"', "OPTIMAL", "O"],
+    4: ["4", "'", "ABANDON", "Q"],
+    5: ["5", "(", "INFO", "INFORMATIONS", "INFORMATION", "HELP", "H", "I"]
+}
+
+RESULT_WORDS = {
+    1: ["1", "RETOUR", "AUTRE", "A", "B", "BACK", "OTHER", "R"],
+    2: ["2", "GAGNÉ", "GAGNE", "TERMINÉ", "TERMINE", "TROUVÉ", "TROUVE", "OK", "Q", "T", "G"]
+}
+
+def lookup_words(word, lookup):
+    for l in lookup.values():
+        if word in l:
+            return list(lookup.keys())[list(lookup.values()).index(l)]
+
+def valid_first_letter(letter):
+    return re.search(f'^[A-Z]$', letter) and re.search(f'^[^KQWXYZ]$', letter)
+
+def valid_length(length):
+    return length <= 10 and length >= 6
+
+def valid_word(word, helper):
+    return len(word) == helper.current_word_len and word[0] == helper.current_word_first
+
+def valid_result(result, helper):
+    return len(result) == helper.current_word_len and re.search(f'^[.RJ]*$', result)
+
+def valid_pool(helper):
+    return len(helper) > 0
+
+def guesser(helper, choice):
     match choice:
-        case "HASARD":
-            return helper.guess()
-        case "MANUEL":
+        case 1:
+            return helper.random_guess()
+        case 2:
+            manual_guess = click.prompt('Inscrivez le mot choisi ').upper()
+            while not valid_word(manual_guess, helper):
+                manual_guess = click.prompt('Veuillez rentrer un mot valide.').upper()
+            click.echo(f'Essayez {manual_guess} !')
             return manual_guess
-        case "OPTIMAL":
-            return "Pas encore implémenté"
+        case 3:
+            print("Pas encore implémenté")
         case _:
             return "Erreur !"
+
+def info_turn(helper):
+    pass
+
+def user_turn(helper):
+    choice = click.prompt('Que faire ? \nHasard [1]    Manuel [2]    Optimal [3]    Abandon [4]    Info [5] ').upper()
+    choice = lookup_words(choice, GUESSER_WORDS)
+    if not choice:
+        click.echo("Veuillez retourner une réponse valide.")
+        return user_turn(helper)
+    if choice == 4:
+        click.echo('Dommage. Une prochaine fois ! :)')
+        return None, None
+    if choice == 5: 
+        info_turn(helper)
+        return user_turn(helper)
+    guess = guesser(helper, choice) 
+    result = click.prompt('Alors ? (RETOUR [1] pour un autre choix, TROUVÉ [2] pour terminer)', type = str).upper()
+    result_temp = lookup_words(result, RESULT_WORDS)
+    if not result_temp:
+        while not valid_result(result, helper):
+            result = click.prompt('Veuillez rentrer un résultat valide. (R pour rouge, J pour jaune, . pour le reste)', type = str).upper()
+        return guess, result
+    elif result_temp == 1:
+        return user_turn(helper)
+    else: #elif result_temp == 2:
+        click.echo(f"Félicitations, vous avez trouvé le mot ! Solution : {guess}")
+        return None, None
 
 def translate(guess, result):
     red = ""
@@ -28,11 +96,18 @@ def translate(guess, result):
     
     return red, yellow, not_there
 
-def first_turn(first_letter, word_length, helper):
+def first_turn(helper):
+    first_letter = click.prompt('Première lettre du mot ', type = str).upper()
+    while not valid_first_letter(first_letter):
+        first_letter = click.prompt('Veuillez entrer une lettre valide. (A à Z sauf K, Q, W, X, Y, Z)', type = str).upper()
+    word_length = click.prompt('Longueur du mot ', type = int)
+    while not valid_length(word_length):
+        word_length = click.prompt('Veuillez entrer une longueur valide. (Entre 6 et 10)', type = int)
     helper.word_length(word_length)
     helper.first_letter(first_letter)
     helper.info()
-    return helper.guess()
+    guess, result = user_turn(helper)
+    return guess, result
 
 def next_turn(helper, guess, result):
     helper.new_turn()
@@ -40,6 +115,11 @@ def next_turn(helper, guess, result):
     helper.red_letters(red)
     helper.yellow_letters(yellow)
     helper.not_in_word(not_there)
+    if not valid_pool(helper):
+        click.echo('OK il y a eu une douille !') #TODO: annuler le changement. Il faut l'état du tour précédent en mémoire
     helper.info()
-    return helper.guess()
+    guess, result = user_turn(helper)
+    return guess, result
 
+def endgame():
+    return click.confirm("Voulez-vous rejouer ?")
